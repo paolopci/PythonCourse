5866387a5a712d5e5ad081fc49ba6a68f5260f39
Paolo Paci <paolopci@yahoo.it>
2025-06-06 16:21:32 +0200

    ho sostituito list con dictionary

--- Bonus/bonus09.py
+++ Bonus/bonus09.py
@@ -2,25 +2,35 @@ password = input("Enter the password: ")
 # verifica le condizioni per la password
 
 
-result = []
+result = {}
 
+# Controlla se la password è più lunga di 8 caratteri
 if len(password) > 8:
-    result.append(True)
+    result['length'] = True
 else:
-    result.append(False)
+    result['length'] = False
 # -------------------------------------------------------------------------------
+# Controlla se la password contiene almeno una cifra
 if any(char.isdigit() for char in password):
-    result.append(True)
+    result['digits'] = True
 else:
-    result.append(False)
+    result['digits'] = False
 # -------------------------------------------------------------------------------
+# Controlla se la password contiene almeno una lettera maiuscola
 if any(char.isupper() for char in password):
-    result.append(True)
+    result['upper-case'] = True
 else:
-    result.append(False)
+    result['upper-case'] = False
 # -------------------------------------------------------------------------------
+print(result)
+# -------------------------------------------------------------------------------
+if all(result.values()):
+    print("Strong Password")
+else:
+    print("Weak Password")
 
-print(all(result))
+
+#    print(all(result))
 
 
 # ------------------------------------------
