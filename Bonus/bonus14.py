9b29e715fcd3cbbccf9dd843962c5d5477d7f508
Paolo Paci <paolopci@yahoo.it>
2025-06-11 12:12:15 +0200

    refactoring files convert feet and inches in cm con modules

--- Bonus/bonus14.py
+++ Bonus/bonus14.py
@@ -1,34 +1,37 @@
+from parsers14 import parse_input
+from convertes14 import convert
+
 
 feet_inches = input("Enter feet and inches: ")
 
 
-def parse_input(feet_inches):
-    try:
-        parts = feet_inches.split(' ')
-        if len(parts) != 2:
-            raise ValueError("Input must be in 'feet inches' format.")
-        feet = float(parts[0])
-        inches = float(parts[1])
-        if feet < 0 or inches < 0:
-            raise ValueError("Feet and inches must be non-negative.")
-        if inches >= 12:
-            raise ValueError("Inches must be less than 12.")
-    except ValueError as e:
-        print(f"Invalid input: {e}")
-        return None, None
+# def parse_input(feet_inches):
+#     try:
+#         parts = feet_inches.split(' ')
+#         if len(parts) != 2:
+#             raise ValueError("Input must be in 'feet inches' format.")
+#         feet = float(parts[0])
+#         inches = float(parts[1])
+#         if feet < 0 or inches < 0:
+#             raise ValueError("Feet and inches must be non-negative.")
+#         if inches >= 12:
+#             raise ValueError("Inches must be less than 12.")
+#     except ValueError as e:
+#         print(f"Invalid input: {e}")
+#         return None, None
 
-    return (feet, inches)
+#     return (feet, inches)
 
 
-def convert(feet, inches):
-    try:
+# def convert(feet, inches):
+#     try:
 
-        centimeters = (feet * 30.48) + (inches * 2.54)
-    except ValueError as e:
-        print(f"Invalid input: {e}")
-        return None
+#         centimeters = (feet * 30.48) + (inches * 2.54)
+#     except ValueError as e:
+#         print(f"Invalid input: {e}")
+#         return None
 
-    return f"{feet} feet and {inches} inches is equal to {centimeters} centimeters."
+#     return f"{feet} feet and {inches} inches is equal to {centimeters} centimeters."
 
 
 (feet, inches) = parse_input(feet_inches)
