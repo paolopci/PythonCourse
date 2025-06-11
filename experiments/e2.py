8787c0e11dbfa1be9e773923d033d929794e6941
Paolo Paci <paolopci@yahoo.it>
2025-06-11 16:06:26 +0200

    csv esempio input city --> temeprature

--- experiments/e2.py
+++ experiments/e2.py
@@ -1,7 +1,18 @@
 import csv
 
+with open('../weather.csv', 'r') as file:
+    reader = csv.reader(file)
+    next(reader)  # Skip the header row
+    for row in reader:
+        print(row)  # Print each row except the header
+
+
+city = input("Enter the city: ")
 
 with open('../weather.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
-        print(row)  # Print each row of the CSV file
+        if row[0].lower() == city.lower():
+            print(f"Weather data for {city}:")
+            print(f"Temperature: {row[1]}°C")
+            break
