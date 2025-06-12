0055c41ac7740b52c6a0e46b6db2f67a61cfaeaa
Paolo Paci <paolopci@yahoo.it>
2025-06-12 10:55:35 +0200

    fix quiz check correct answer with number

--- Bonus/bonus15Bis.py
+++ Bonus/bonus15Bis.py
@@ -7,7 +7,12 @@ with open('questions.json', 'r') as file:
 
 data = json.loads(content)
 
-print(type(content))
-print(type(data))
-
-print(data)
+for item in data:
+    print(item["question"])
+    for index, option in enumerate(item["alternatives"]):
+        print(f"{index+1} - {option}")
+    answer = int(input("Your answer: "))
+    if answer == int(item["correct_answer"]):
+        print("Correct!")
+    else:
+        print(f"Wrong! The correct answer is: {item['correct_answer']}")
