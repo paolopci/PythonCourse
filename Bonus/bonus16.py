d3a7cd25252c7da9323f6814eb71d0d7e356138e
Paolo Paci <paolopci@yahoo.it>
2025-06-13 12:02:55 +0200

    implemento zip service (module)

--- Bonus/bonus16.py
+++ Bonus/bonus16.py
@@ -3,11 +3,11 @@ import FreeSimpleGUI as sg
 
 label1 = sg.Text('Select files to compress:')
 input1 = sg.Input()
-choose_btn1 = sg.FilesBrowse("Choose")
+choose_btn1 = sg.FilesBrowse("Choose", key='files')
 
 label2 = sg.Text('Select destination folder:')
 input2 = sg.Input()
-choose_btn2 = sg.FolderBrowse("Choose")
+choose_btn2 = sg.FolderBrowse("Choose", key='folder')
 
 compress_btn = sg.Button('Compress')
 
@@ -17,5 +17,12 @@ window = sg.Window('Files Compressor', layout=[
                        label2, input2, choose_btn2], [compress_btn]
                    ])
 
-window.read()
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values["files"].split(';')
+    folder = values['folder']
+   # print(filepaths, folder)
+
+
 window.close()
