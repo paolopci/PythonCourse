0fe87015c5f355b61e9bd5d2ebd7427b4ef1ab4e
Paolo Paci <paolopci@yahoo.it>
2025-06-14 15:45:07 +0200

    step2 : test zip_extractor.py

--- Bonus/zip_extractor.py
+++ Bonus/zip_extractor.py
@@ -1,18 +1,22 @@
 import zipfile
 
 
-def extract_zip(zip_path, extract_to):
+def extract_zip(zip_path, dest_dir):
     """
     Extracts a zip file to a specified directory.
 
     :param zip_path: Path to the zip file.
-    :param extract_to: Directory where the contents will be extracted.
+    :param dest_dir: Directory where the contents will be extracted.
     """
     try:
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-            zip_ref.extractall(extract_to)
-        print(f"Extraction complete: {zip_path} -> {extract_to}")
+        with zipfile.ZipFile(zip_path, 'r') as archive:
+            archive.extractall(dest_dir)
+        print(f"Extraction complete: {zip_path} -> {dest_dir}")
     except zipfile.BadZipFile:
         print(f"Error: {zip_path} is not a valid zip file.")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+
+# if __name__ == "__main__":
+#     extract_zip('./dest/compressed.zip', './aaa')
